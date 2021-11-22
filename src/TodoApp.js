3559fd9b5dd63a0c3dcda0a4977d014e2ef2fc9b
S-P-RAJAT <rajathsiddeshwar@gmail.com>
2021-11-23 01:11:23 +0530

    ADDED: Todo redux app files

---
+++ src/TodoApp.js
@@ -0,0 +1,15 @@
+import React from "react";
+import AddTodo from "./components/AddTodo";
+import TodoList from "./components/TodoList";
+import MyList from "./components/MyList";
+import "./TodoApp.css";
+
+export default function TodoApp() {
+  return (
+    <div className="todo-app">
+      <h1>Todo List</h1>
+      <AddTodo />
+      <MyList />
+    </div>
+  );
+}
